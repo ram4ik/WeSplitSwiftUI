cf036c11cca1b718ffc4cbd0e03b51db585a936e
ram4ik <ram4ik@hotmail.com>
2023-10-13 09:23:09 -0700

    Initial Commit

---
+++ WeSplitSwiftUI/WeSplitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitSwiftUIApp.swift
+//  WeSplitSwiftUI
+//
+//  Created by admin on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
